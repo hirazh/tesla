f9e2b1470f0613049d07d10d1e89f76f91abff83
Hiras <mhmdhiras@gmail.com>
2018-05-27 17:50:24 +0530

    created the index page

--- components/Nav.vue
+++ components/Nav.vue
@@ -1,49 +1,46 @@
 <template>
-    
-        <div class="columns is-marginless">
-            <div class="column navbar-start">
-                <span class="navbar-item">
-                    <h1 class="title is-4 brand-name">T E S L A</h1>
-                </span>
-            </div>
-            <div class="column middle-nav">
-                <span class="navbar-item">MODEL S</span>
-                <span class="navbar-item">MODEL X</span>
-                <span class="navbar-item">MODEL 3</span>
-                <span class="navbar-item">ROADSTER</span>
-            </div>
-            <div class="column navbar-end">
-                <span class="navbar-item">SHOP</span>
-                <span class="navbar-item">SIGN IN</span>
-                <span class="navbar-item"><i class="fas fa-bars"></i></span>
-            </div>
+    <div class="columns is-marginless">
+        <div class="column navbar-start nav-column">
+            <span class="navbar-item">
+                <h1 class="title is-4 brand-name">T E S L A</h1>
+            </span>
         </div>
+        <div class="column middle-nav nav-column">
+            <span class="navbar-item">MODEL S</span>
+            <span class="navbar-item">MODEL X</span>
+            <span class="navbar-item">MODEL 3</span>
+            <span class="navbar-item">ROADSTER</span>
+        </div>
+        <div class="column navbar-end nav-column">
+            <span class="navbar-item">SHOP</span>
+            <span class="navbar-item">SIGN IN</span>
+            <span class="navbar-item"><i class="fas fa-bars"></i></span>
+        </div>
+    </div>
        
 </template>
 
 <script>
-export default {
-    
-}
+export default {};
 </script>
 
 <style>
-
-.column {
-    display: flex;
-    flex-direction: row;
-    background-color: #4F4F4F;
-    height: 10vh;
+.nav-column {
+  display: flex;
+  flex-direction: row;
+  background-color: #4f4f4f;
+  height: 10vh;
 }
 .middle-nav {
-    justify-content: center;
+  justify-content: center;
 }
 
 .navbar-item {
-    color: #f0f0f0;
+  color: #f0f0f0;
 }
 
 .brand-name {
-    color: #f0f0f0;
+  color: #f0f0f0;
 }
+
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,36 @@
 <template>
   <section class="hero">
-    <h1 class="title">Hi a  Homeapage</h1>
+    <div class="container main-container">
+      <p class="title is-size-1">Quickest Acceleration.<br> Longest Range. The Safest Cars Ever.</p>
+      <div class="container tesla-model-wrapper">
+        <div class="columns">
+          <div class="column">
+            <a href="#"><div class="tesla-models">MODEL S</div></a>
+          </div>
+          <div class="column">
+            <a href="#"><div class="tesla-models">MODEL X</div></a>
+          </div>
+          <div class="column">
+            <a href="#"><div class="tesla-models">MODEL 3</div></a>
+          </div>
+          <div class="column">
+            <a href="#"><div class="tesla-models">ROADSTER</div></a>
+          </div>
+        </div>
+      </div>
+      <p class="title is-size-6 news">GET NEWSLETTER ></p>
+    </div>
+
+    <div class="container footer-part">
+      <a href="#">Tesla © 2018</a>|
+      <a href="#">Privacy & Legal</a>|
+      <a href="#">Contact</a>|
+      <a href="#">Careers</a>|
+      <a href="#">Get Newsletter</a>|
+      <a href="#">Forums</a>|
+      <a href="#">Locations</a>|
+      <a href="#"><img src="../assets/images/usa-flag.png"></a>
+    </div>
   </section>
 </template>
 
@@ -14,7 +44,53 @@ export default {
 
 <style>
 .hero {
-  background-color: darkcyan;
+  background-image: url('../assets/images/tesla-model-3.jpg');
+  background-size: cover;
   height: 90vh;
 }
+
+.is-size-1 {
+  color: #ffffff;
+  padding: 4%;
+  font-weight: 100;
+  line-height: 150%;
+}
+
+.main-container {
+  text-align: center;
+}
+
+.tesla-model-wrapper {
+  display: flex;
+  justify-content: space-around;
+}
+
+.tesla-models {
+  background-color: black;
+  opacity: 0.7;
+  color: #ffffff;
+  width: 180px;
+  padding: 3%;
+  
+}
+
+.news {
+  color: #ffffff;
+  padding: 5%;
+}
+
+.footer-part {
+  color: #ffffff;
+  display: flex;
+  justify-content: center;
+  margin-bottom: 3%;
+}
+
+.footer-part a {
+  text-decoration: none;
+  color: #f0f0f0;
+  padding: 0 1% 0 1%;
+  
+}
+
 </style>
